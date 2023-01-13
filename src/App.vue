57b1131c6edd28c769fa84dd921a7c08107c4201
ritveek-rana <ritveekrana@gmail.com>
2023-01-14 05:18:07 +0530

    template commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from "./components/Navbar.vue"
+import blogview from "./components/blogview.vue"
+import blogpage from "./components/blogview.vue"
+
+</script>
+
+<template>
+  <Navbar/>
+  <blogview/>
+  <blogpage/>
+</template>
+
